9fc300a72484bb56ef462aa2f2ed38af04081d10
Samuel Sihotang <samsihotang1@gmail.com>
2024-05-23 20:00:30 +0700

    add news

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,11 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class NewsController extends Controller
 {
     public function index()
     {
         return view('news');
+    }
+
+
+
+    public function create()
+    {
+        return view('createnews');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $path = $image->storeAs('img/news/' . $slug, $key . '.jpg', 'public');
+        }
+
+        News::create([
+            'title' => $validatedData['name'],
+            'description' => $validatedData['description'],
+            'image' => $path,
+        ]);
+        return redirect()->route('homestay', $slug);
     }
 }
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -28,211 +28,49 @@
                 <h3 class="text-center padding-bottom-small">Artikel Terbaru</h3>
             </div>
             <div class="section clearfix"></div>
-            <div class="col-md-6 col-xl-4">
+            <div class="col-md-6 col-xl-6">
                 <div class="room-box background-white">
                     <img src="img/blog/1.jpg" alt="">
                     <div class="room-box-in">
                         <h6 class="">WE WANT TO SHARE WITH YOU OUR MOOD</h6>
                         <p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo
                             inventore veritatis et.</p>
-                        <a class="mt-1 btn btn-primary" href="post.html">read more</a>
-                        <div class="room-icons news-tags mt-4 pt-4">
-                            <a href="#">luxury</a>
-                            <a href="#">vacation</a>
-                            <a href="#">travel</a>
-                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4 mt-4 mt-md-0">
+            <div class="col-md-6 col-xl-6">
                 <div class="room-box background-white">
-                    <img src="img/blog/2.jpg" alt="">
+                    <img src="img/blog/1.jpg" alt="">
                     <div class="room-box-in">
-                        <h6 class="">SLEEP, CODE, EAT, TRAVEL. REPEAT.</h6>
+                        <h6 class="">WE WANT TO SHARE WITH YOU OUR MOOD</h6>
                         <p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo
                             inventore veritatis et.</p>
-                        <a class="mt-1 btn btn-primary" href="post.html">read more</a>
-                        <div class="room-icons news-tags mt-4 pt-4">
-                            <a href="#">luxury</a>
-                            <a href="#">vacation</a>
-                            <a href="#">travel</a>
-                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4 mt-4 mt-xl-0">
+            <div class="col-md-6 col-xl-6">
                 <div class="room-box background-white">
-                    <img src="img/blog/3.jpg" alt="">
+                    <img src="img/blog/1.jpg" alt="">
                     <div class="room-box-in">
-                        <h6 class="">THE GOLDEN RULE OF WEBDESIGN.</h6>
+                        <h6 class="">WE WANT TO SHARE WITH YOU OUR MOOD</h6>
                         <p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo
                             inventore veritatis et.</p>
-                        <a class="mt-1 btn btn-primary" href="post.html">read more</a>
-                        <div class="room-icons news-tags mt-4 pt-4">
-                            <a href="#">luxury</a>
-                            <a href="#">vacation</a>
-                            <a href="#">travel</a>
-                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4 mt-4">
+            <div class="col-md-6 col-xl-6">
                 <div class="room-box background-white">
-                    <img src="img/blog/4.jpg" alt="">
+                    <img src="img/blog/1.jpg" alt="">
                     <div class="room-box-in">
-                        <h6 class="">PRETTY MUCH HOW YOU'LL SEE IT.</h6>
+                        <h6 class="">WE WANT TO SHARE WITH YOU OUR MOOD</h6>
                         <p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo
                             inventore veritatis et.</p>
-                        <a class="mt-1 btn btn-primary" href="post.html">read more</a>
-                        <div class="room-icons news-tags mt-4 pt-4">
-                            <a href="#">luxury</a>
-                            <a href="#">vacation</a>
-                            <a href="#">travel</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-4 mt-4">
-                <div class="room-box background-white">
-                    <img src="img/blog/5.jpg" alt="">
-                    <div class="room-box-in">
-                        <h6 class="">FORM AND CONTENT TOGETHER.</h6>
-                        <p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo
-                            inventore veritatis et.</p>
-                        <a class="mt-1 btn btn-primary" href="post.html">read more</a>
-                        <div class="room-icons news-tags mt-4 pt-4">
-                            <a href="#">luxury</a>
-                            <a href="#">vacation</a>
-                            <a href="#">travel</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-4 mt-4">
-                <div class="room-box background-white">
-                    <img src="img/blog/6.jpg" alt="">
-                    <div class="room-box-in">
-                        <h6 class="">DON´T GIVE UP, KEEP ON FOCUS.</h6>
-                        <p class="mt-3">Sed ut perspiciatis unde omnis, totam rem aperiam, eaque ipsa quae ab illo
-                            inventore veritatis et.</p>
-                        <a class="mt-1 btn btn-primary" href="post.html">read more</a>
-                        <div class="room-icons news-tags mt-4 pt-4">
-                            <a href="#">luxury</a>
-                            <a href="#">vacation</a>
-                            <a href="#">travel</a>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-<div class="section padding-bottom-smaller z-bigger background-grey over-hide">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="project-nav-wrap">
-                    <a href="#">
-                        <div class="left-nav" data-scroll-reveal="enter left move 60px over 0.9s after 0.1s">new<div
-                                class="text-on-hover">latest news</div>
-                        </div>
-                    </a>
-                    <a href="#">
-                        <div class="right-nav" data-scroll-reveal="enter right move 60px over 0.9s after 0.1s">old
-                            <div class="text-on-hover">older news</div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="section background-dark over-hide">
-    <div class="container-fluid py-4">
-        <div class="row">
-            <div class="col-sm-6 col-lg-3">
-                <a href="services.html">
-                    <div class="img-wrap services-wrap">
-                        <img src="img/ser-1.jpg" alt="">
-                        <div class="services-text-over">spa salon</div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-sm-6 col-lg-3 pt-4 pt-sm-0">
-                <a href="services.html">
-                    <div class="img-wrap services-wrap">
-                        <img src="img/ser-2.jpg" alt="">
-                        <div class="services-text-over">restaurant</div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-sm-6 col-lg-3 pt-4 pt-lg-0">
-                <a href="services.html">
-                    <div class="img-wrap services-wrap">
-                        <img src="img/ser-3.jpg" alt="">
-                        <div class="services-text-over">pool</div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-sm-6 col-lg-3 pt-4 pt-lg-0">
-                <a href="services.html">
-                    <div class="img-wrap services-wrap">
-                        <img src="img/ser-4.jpg" alt="">
-                        <div class="services-text-over">activities</div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="section padding-top-bottom-small background-black over-hide footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 text-center text-md-left">
-                <img src="img/logo.png" alt="">
-                <p class="color-grey mt-4">Avenue Street 3284<br>Thessaloniki</p>
-            </div>
-            <div class="col-md-4 text-center text-md-left">
-                <h6 class="color-white mb-3">information</h6>
-                <a href="tandc.html">terms &amp; conditions</a>
-                <a href="services.html">services</a>
-                <a href="restaurant.html">restaurant</a>
-                <a href="testimonials.html">testimonials</a>
-                <a href="gallery.html">gallery &amp; images</a>
-            </div>
-            <div class="col-md-5 mt-4 mt-md-0 text-center text-md-left logos-footer">
-                <h6 class="color-white mb-3">about us</h6>
-                <p class="color-grey mb-4">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
-                    praesentium voluptatum deleniti atque corrupti quos dolores.</p>
-                <img src="img/logos/1.png" alt="">
-                <img src="img/logos/2.png" alt="">
-                <img src="img/logos/3.png" alt="">
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="section py-4 background-dark over-hide footer-bottom">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 text-center text-md-left mb-2 mb-md-0">
-                <p>2019 © Thalia. All rights reserved.</p>
-            </div>
-            <div class="col-md-6 text-center text-md-right">
-                <a href="#" class="social-footer-bottom">facebook</a>
-                <a href="#" class="social-footer-bottom">twitter</a>
-                <a href="#" class="social-footer-bottom">instagram</a>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<div class="scroll-to-top"></div>
-
 
 
 
